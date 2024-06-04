ecd31bbf96d4e8532a3f44f7fda2fa78dfd8ea5e
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-06-04 19:15:01 +0530

    initial commit

---
+++ backend/db/session.py
@@ -0,0 +1,9 @@
+from core.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+SQL_ALCHEMY_DATABASE_URL = settings.DATABASE_URL
+print("Database URL: ", SQL_ALCHEMY_DATABASE_URL)
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
+
+SESSIONLOCAL = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
 from core.config import settings
+from db.base_class import Base
+from db.session import engine
+from fastapi import FastAPI
 
-app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
 
 
 @app.get("/")
