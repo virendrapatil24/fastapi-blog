b242ede395058e0fe03e926b0d7b0a1ef90690b2
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-06-04 20:02:26 +0530

    setup alembic

--- backend/db/session.py
+++ backend/db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 SQL_ALCHEMY_DATABASE_URL = settings.DATABASE_URL
-print("Database URL: ", SQL_ALCHEMY_DATABASE_URL)
+# print("Database URL: ", SQL_ALCHEMY_DATABASE_URL)
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
 SESSIONLOCAL = sessionmaker(autocommit=False, autoflush=False, bind=engine)
